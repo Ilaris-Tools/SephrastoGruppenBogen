cbe88f1e4ff59255b4d25d20a2d2bcf41758d76f
Lukas <mail@sarbot.de>
2024-11-02 21:30:28 +0100

    more layout

--- Gruppe.py
+++ Gruppe.py
@@ -22,14 +22,37 @@ class Gruppe:
         html = html.replace("{name}", char.name)
         html = html.replace("{kurzbeschreibung}", char.kurzbeschreibung)
         rows = []
+        # werte
+        wtab = "<table class='werte'><tr>"
+        for a in char.attribute.values():
+            wtab += f"<td>{a.name} {a.wert}</td>"
+        wtab += "</tr><tr>"
+        skipwerte = ["DH", "SB", "SchiP", "RS"]
+        print(list(char.abgeleiteteWerte.keys()))
+        for w in char.abgeleiteteWerte.values():
+            if w.name in skipwerte:
+                continue
+            # if w.name == "WS" and "RS" in char.abgeleiteteWerte:
+            #     wtab += f"<td>{w.name} {w.wert}/{w.wert + char.abgeleiteteWerte['RS'].wert} </td>"
+            #     continue
+            wtab += f"<td>{w.name} {w.wert}</td>"
+        wtab += "</tr></table>"
+        rows.append(f'<div class="row">{wtab}</div>')
+        # fertigkeiten
+        ftab = '<table class="fertigkeiten">'
+        for fert in char.fertigkeiten.values():
+            tal = ""
+            # tal = ",".join(fert.talente)
+            # if tal:
+            #     tal = f" ({tal})"
+            ftab += f"<tr><td>{fert.name}{tal}</td><td class='pw'>{fert.probenwert}/{fert.probenwertTalent}</td></tr>"
+        ftab += "</table>"
+        rows.append(f'<div class="row">{ftab}</div>')
         # vorteile
         vorteile = ", ".join([v.anzeigenameExt for v in char.vorteile.values()])
         rows.append(f'<div class="row"><b>Vorteile:</b> {vorteile}</div>')
-        # fertigkeiten
-        fertigkeiten = ", ". join([f"{f.name} ({f.probenwert}/{f.probenwertTalent})" for f in char.fertigkeiten.values()])
-        rows.append(f'<div class="row"><b>Fertigkeiten: </b>{fertigkeiten}</div>')
         # close char
-        html = html.replace("{rows}", "<hr/>".join(rows))
+        html = html.replace("{rows}", "".join(rows))
         return html
     
     def toHtml(self, charaktere=[]):
@@ -39,10 +62,12 @@ class Gruppe:
         with open(os.path.join(folder, "assets", "template.html"), "r") as f:
             html = f.read()
         chars = [self.char2html(char) for char in charaktere]
+        assets = os.path.join(folder, "assets")
         backgroundImg = os.path.join(folder, "assets", "Hintergrund.jpg")
         backgroundImg = backgroundImg.replace("\\", "/")
         html = html.replace("{css}", css)
         html = html.replace("{charaktere}", "".join(chars))
         html = html.replace("{name}", self.name)
         html = html.replace("{backgroundImg}", backgroundImg)
+        html = html.replace("{assets}", assets)
         return html
